5a484241788381431c32e97564868d9f4a91f920
shivam saraswat <shivam@gmail.com>
2022-07-21 22:39:13 +0530

    mongo

--- server.js
+++ server.js
@@ -11,7 +11,10 @@ process.on('uncaughtException', (err) => {
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
-const DB = process.env.DATABASE;
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
 mongoose.connect(DB).then(() => {
   console.log('DB connected successfully...');
 });
